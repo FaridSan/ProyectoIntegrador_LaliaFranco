8bc35ead3a782e0cee368e4b861ca78c4a188f2f
Franco <francolalia97@gmail.com>
2022-05-20 20:54:53 -0300

    Se terminaron las redes falta poner links

--- ProyectoIntegrador/src/app/app.module.ts
+++ ProyectoIntegrador/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
+import { RedesSocComponent } from './components/redes-soc/redes-soc.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    LogoAPComponent
+    LogoAPComponent,
+    RedesSocComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
