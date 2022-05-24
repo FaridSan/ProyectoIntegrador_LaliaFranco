67dce7ce79628b0e4870ac4d85bf98dc48e7872a
Franco <francolalia97@gmail.com>
2022-05-24 12:48:16 -0300

    se termino proyectos

--- ProyectoIntegrador/src/app/app.module.ts
+++ ProyectoIntegrador/src/app/app.module.ts
@@ -12,6 +12,7 @@ import { ExperienciaComponent } from './components/experiencia/experiencia.compo
 import { EducacionComponent } from './components/educacion/educacion.component';
 import { NgCircleProgressModule } from 'ng-circle-progress';
 import { HasComponent } from './components/has/has.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
 
 @NgModule({
   declarations: [
@@ -23,7 +24,8 @@ import { HasComponent } from './components/has/has.component';
     AcercaDeComponent,
     ExperienciaComponent,
     EducacionComponent,
-    HasComponent
+    HasComponent,
+    ProyectosComponent
   ],
   imports: [
     BrowserModule,
